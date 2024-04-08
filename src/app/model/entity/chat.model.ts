27aa9ac044354851ed4c0f9b388e0df3de75fb5d
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-04-09 00:26:49 +0300

    refactor

--- src/app/model/entity/chat.model.ts
+++ src/app/model/entity/chat.model.ts
@@ -7,16 +7,16 @@ export class ChatModel {
   public userName: string; // Имя пользователя
   public messageList: MessageModel[]; // Список сообщений
   public status: string; // Статус чата
-  public createDate: string; // Дата создания
-  public updateDate: string; // Дата обновления
+  public createDate: Date; // Дата создания
+  public updateDate: Date; // Дата обновления
 
   constructor(id: number,
               supportName: string,
               userName: string,
               messageList: MessageModel[],
               status: string,
-              createDate: string,
-              updateDate: string) {
+              createDate: Date,
+              updateDate: Date) {
     this.id = id;
     this.supportName = supportName;
     this.userName = userName;
--- src/app/model/entity/message.model.ts
+++ src/app/model/entity/message.model.ts
@@ -1,16 +1,18 @@
+import {DatePipe} from "@angular/common";
+
 export class MessageModel {
 
   public id: number; // Идентификатор сообщения
   public chatId: number; // Идентификатор чата
   public senderName: string; // Имя отправителя
   public content: string; // Содержимое сообщения
-  public createDate: string; // Дата создания сообщения
+  public createDate: DatePipe; // Дата создания сообщения
 
   constructor(id: number,
               chatId: number,
               senderName: string,
               content: string,
-              createDate: string) {
+              createDate: DatePipe) {
     this.id = id;
     this.chatId = chatId;
     this.senderName = senderName;
